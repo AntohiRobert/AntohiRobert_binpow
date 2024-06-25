1909d5a30eaea22d4073b780159bceb162e6c9ab
Antohi Robert <antohir@gmail.com>
2024-06-26 00:12:40 +0300

    files

---
+++ binpow.cpp
@@ -0,0 +1,18 @@
+module;
+#include <vector>
+export module AntohiRobert_binpow;
+
+export namespace AntohiRobert_binpow
+{
+    long long binpow(long long a, long long b, long long m) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+}
